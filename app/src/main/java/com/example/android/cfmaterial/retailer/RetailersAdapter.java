354267a2a52bfdd25dbafb655be2edc453d2827c
sassoni <Chrysanthi.Chaleva@catalinamarketing.com>
2015-05-15 10:44:49 -0400

    Cleanup

--- app/src/main/java/com/example/android/cfmaterial/retailer/RetailersAdapter.java
+++ app/src/main/java/com/example/android/cfmaterial/retailer/RetailersAdapter.java
@@ -1,55 +1,30 @@
 package com.example.android.cfmaterial.retailer;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.android.cfmaterial.R;
 
 public class RetailersAdapter extends ArrayAdapter<Retailer> {
 
     private Context context;
+    private Retailer[] retailers;
 
-    private Retailer[] retailers = new Retailer[]{
-            new Retailer("A&P", R.drawable.i_aandp),
-            new Retailer("Family Express", R.drawable.i_familyexpress),
-            new Retailer("Giant Carlisle", R.drawable.i_gc),
-            new Retailer("Giant Landover", R.drawable.i_gl),
-            new Retailer("Harris Teeter", R.drawable.i_harristeeter),
-            new Retailer("Kroger", R.drawable.i_kroger),
-            new Retailer("Marsh", R.drawable.i_marsh),
-            new Retailer("Martin's", R.drawable.i_martins),
-            new Retailer("Safeway", R.drawable.i_safeway),
-            new Retailer("ShopRite", R.drawable.i_shoprite),
-            new Retailer("Stop&Shop", R.drawable.i_stopandshop),
-            new Retailer("Walmart", R.drawable.i_walmart),
-            new Retailer("Weis", R.drawable.i_weis)
-    };
-
-    public RetailersAdapter(Context context) {
-        super(context, 0);
+    public RetailersAdapter(Context context, Retailer[] retailers) {
+        super(context, 0, retailers);
         this.context = context;
+        this.retailers = retailers;
     }
-
-//    public void addData() {
-////        for (Retailer retailer : retailers) {
-////            add(retailer);
-////        }
-//    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Log.i("RETAILERSADAPTER", "getView");
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.retailers_gridview_tile, parent, false);
-       // TextView textView = (TextView) view.findViewById(R.id.retailers_gridview_tile_title);
         ImageView imageView = (ImageView) view.findViewById(R.id.retailers_gridview_tile_image);
-        //textView.setText(retailers[position].getName());
         imageView.setImageResource(retailers[position].getDrawableId());
         return view;
     }
--- app/src/main/java/com/example/android/cfmaterial/retailer/RetailersFragment.java
+++ app/src/main/java/com/example/android/cfmaterial/retailer/RetailersFragment.java
@@ -1,164 +1,82 @@
 package com.example.android.cfmaterial.retailer;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Rect;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.cfmaterial.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RetailersFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RetailersFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RetailersFragment extends Fragment {
 
-//    private RecyclerView recyclerView;
-//    private RecyclerView.Adapter adapter;
-//    private RecyclerView.LayoutManager layoutManager;
-    private GridView gridView;
+    private Retailer[] retailers = new Retailer[]{
+            new Retailer("A&P", R.drawable.i_aandp),
+            new Retailer("Family Express", R.drawable.i_familyexpress),
+            new Retailer("Giant Carlisle", R.drawable.i_gc),
+            new Retailer("Giant Landover", R.drawable.i_gl),
+            new Retailer("Harris Teeter", R.drawable.i_harristeeter),
+            new Retailer("Kroger", R.drawable.i_kroger),
+            new Retailer("Marsh", R.drawable.i_marsh),
+            new Retailer("Martin's", R.drawable.i_martins),
+            new Retailer("Safeway", R.drawable.i_safeway),
+            new Retailer("ShopRite", R.drawable.i_shoprite),
+            new Retailer("Stop&Shop", R.drawable.i_stopandshop),
+            new Retailer("Walmart", R.drawable.i_walmart),
+            new Retailer("Weis", R.drawable.i_weis)
+    };
 
-    private Context context;
-
-    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
-        private int space;
-
-        public SpacesItemDecoration(int space) {
-            this.space = space;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view,
-                                   RecyclerView parent, RecyclerView.State state) {
-            outRect.left = space;
-            outRect.right = space;
-            outRect.bottom = space;
-
-            // Add top margin only for the first item to avoid double space between items
-            if(parent.getChildPosition(view) == 0)
-                outRect.top = space;
-        }
+    public interface OnRetailerClickedListener {
+        public void onRetailerClicked(Retailer retailer);
     }
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private OnRetailerClickedListener listener;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReatailersFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RetailersFragment newInstance(/*String param1, String param2*/) {
-        RetailersFragment fragment = new RetailersFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-        return fragment;
+    public static RetailersFragment newInstance() {
+        return new RetailersFragment();
     }
 
     public RetailersFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.i("RETAILERSFRAGMENT", "onCreate");
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_retailers, container, false);
-//        recyclerView = (RecyclerView) view.findViewById(R.id.fragment_retailers_recycler_view);
-//        layoutManager = new GridLayoutManager(getActivity(), 3);
-//        recyclerView.setLayoutManager(layoutManager);
-//
-//        // specify an adapter (see also next example)
-//        adapter = new RetailersRecyclerAdapter();
-//        recyclerView.setAdapter(adapter);
-//
-//        //int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.spacing);
-//        recyclerView.addItemDecoration(new SpacesItemDecoration(5));
 
-        gridView = (GridView) view.findViewById(R.id.fragment_retailers_gridview);
-        RetailersAdapter adapter = new RetailersAdapter(getActivity());
+        RetailersAdapter adapter = new RetailersAdapter(getActivity(), retailers);
+
+        GridView gridView = (GridView) view.findViewById(R.id.fragment_retailers_gridview);
         gridView.setAdapter(adapter);
-       // adapter.addData();
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (listener != null) {
+                    listener.onRetailerClicked(retailers[position]);
+                }
+            }
+        });
 
         return view;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        this.context = context;
-//        try {
-//            mListener = (OnFragmentInteractionListener) activity;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(activity.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
+        try {
+            listener = (OnRetailerClickedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnRetailerClickedListener");
+        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        listener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
 }
