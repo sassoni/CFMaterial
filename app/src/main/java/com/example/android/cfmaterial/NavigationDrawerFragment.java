736d50afc60e2ba13636dd08922a5399a07de3cf
sassoni <Chrysanthi.Chaleva@catalinamarketing.com>
2015-05-20 17:47:54 -0400

    Modifying titles to toolbar

--- app/src/main/java/com/example/android/cfmaterial/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/android/cfmaterial/NavigationDrawerFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 public abstract class NavigationDrawerFragment extends Fragment {
 
     abstract public void setupNavigationDrawer();
+    abstract public void setupToolbar();
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setupNavigationDrawer();
+        setupToolbar();
     }
 }
